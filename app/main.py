8225d136c4b25f67a59e749a9a645cae140912dd
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-06 07:26:56 +0900

    feat: chat endpoint sample

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
+from app.api import router as api_router
+
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"message": "Hello World"}
+app.include_router(api_router, prefix="/api")
