1b3493dc653cf864240447d23928edf70aea454b
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-06 09:01:16 +0900

    feat: 環境変数設定

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 from app.api import router as api_router
+
+
+load_dotenv()
+
+# 環境変数を読み取る例
+api_key = os.environ["OPENAI_API_KEY"]
 
 app = FastAPI()
 
